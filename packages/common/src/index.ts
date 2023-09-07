60217e3619b95287f85fb4eb35ff5144eb1fc8fb
Rishabh <rishabhh.singh.rs@gmail.com>
2023-09-07 21:52:38 +0530

    common file

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const UserInput = z.object({
-    email: z.string(),
-    password: z.string()
-})
+  email: z.string().min(1),
+  password: z.string().min(1),
+});
