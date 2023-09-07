5b5dcbe41de576efc2215695c3413d50425d4b60
Rishabh <rishabhh.singh.rs@gmail.com>
2023-09-07 20:47:33 +0530

    yarn to npm

--- apps/client-next/deploy.sh
+++ apps/client-next/deploy.sh
@@ -3,6 +3,6 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd /home/ubuntu/monorepo
 git pull origin master
-yarn build
+npm run build
 pm2 stop next
 pm2 start npm --name "next" -- run "start:next"
--- apps/client-react/deploy.sh
+++ apps/client-react/deploy.sh
@@ -3,6 +3,6 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd /home/ubuntu/monorepo
 git pull origin master
-yarn build
+npm run build
 pm2 stop react
 pm2 start npm --name "react" -- run "start:react"
