84d93f6ff97cb106ac9efbdaded3628add2903d5
Rishabh <rishabhh.singh.rs@gmail.com>
2023-09-08 13:55:49 +0530

    deploy 1

--- apps/client-next/deploy.sh
+++ apps/client-next/deploy.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd /home/ubuntu/monorepo
-git pull origin master
+sudo git pull origin master
 yarn install
 yarn build
 pm2 stop next
